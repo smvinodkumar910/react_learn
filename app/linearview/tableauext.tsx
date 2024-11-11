890e39f30d15f4ac28c0676b1b7d7c291dc4eba3
Vinodkumar Madhavan <smvinodkumar910@gmail.com>
2024-11-11 07:13:55 +0000

    changes

--- app/linearview/tableauext.tsx
+++ app/linearview/tableauext.tsx
@@ -2,16 +2,22 @@
 import React, { useEffect } from 'react';
 
 
+
 function MainComponent () {
 
   const [selectedSheetName, setSelectedSheetName] = React.useState<string | null>(null);
+  const [dashboardName, setDashboardName] = React.useState<string | null>(null);
   
   useEffect(() => {
     if (typeof window !== 'undefined' && window.tableau) {
       window.tableau.extensions.initializeAsync().then(() => {
         try {
+          const dashboard = window.tableau.extensions.dashboardContent?.dashboard;
+          const dashboardName = dashboard?.name
           const selectedSheet = window.tableau.extensions.settings.get('sheet');
+          
           setSelectedSheetName(typeof selectedSheet === 'string' ? selectedSheet : null);
+          setDashboardName(typeof dashboardName === 'string' ? dashboardName : null);
         } catch (error) {
           console.error("Error getting Tableau settings:", error);
           setSelectedSheetName(null);
@@ -23,7 +29,7 @@ function MainComponent () {
 
   const renderSheet = () => {
     if (selectedSheetName) {
-      return <div>Selected Sheet: {selectedSheetName}</div>;
+      return <div>Selected Sheet: {selectedSheetName} in dashboard {dashboardName}</div>;
     }
     return <div>No sheet selected</div>;
   };
--- app/tableau.d.ts
+++ app/tableau.d.ts
@@ -1,8 +1,14 @@
 export {};
 
+import * as TableauExtensions from '@tableau/extensions-api-types/ExternalContract/Extensions/Namespaces/Tableau';
+import * as TableauShared from '@tableau/extensions-api-types/ExternalContract/Shared/Namespaces/Tableau';
+
+type Tableau = typeof TableauExtensions & typeof TableauShared;
+
+
 declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    tableau: any;
+    tableau: Tableau;
   }
 }
