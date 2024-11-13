bd707006b4edc45aabcb59aff2374d8a8aa8d1f1
Vinodkumar Madhavan <smvinodkumar910@gmail.com>
2024-11-13 19:09:14 +0530

    changes

--- app/linearview/tableauext.tsx
+++ app/linearview/tableauext.tsx
@@ -27,27 +27,27 @@ function getDataColumns(worksheet: marks.Worksheet){
 */
 
 function getData(worksheet: marks.Worksheet): { from: string | undefined; to: string | undefined; }[] {
-  const records: { from: string | undefined; to: string | undefined; }[] =[];
-  worksheet.getSummaryDataReaderAsync().then((response)=>{
+  const records: { from: string | undefined; to: string | undefined; }[] = [];
+  worksheet.getSummaryDataReaderAsync().then((response) => {
     response.getAllPagesAsync().then(data => {
       console.log(data.totalRowCount);
-      
+
       data.data.forEach((value) => {
         const fromValue = value.at(1);
         const toValue = value.at(2);
-        const record: {from: string | undefined; to: string | undefined;} = {from: undefined, to: undefined};
-        if(fromValue){
+        const record: { from: string | undefined; to: string | undefined; } = { from: undefined, to: undefined };
+        if (fromValue) {
           record.from = fromValue.formattedValue;
         }
-        if(toValue){
+        if (toValue) {
           record.to = toValue.formattedValue;
         }
         records.push(record);
-        
-      }) 
-      
+
+      })
+
     })
-  },(error) => console.log(error));
+  }, (error) => console.log(error));
   console.log(records);
   return records;
 }
@@ -58,13 +58,13 @@ function getData(worksheet: marks.Worksheet): { from: string | undefined; to: st
 function getFilterDetails(worksheet: marks.Worksheet): void {
   worksheet.getFiltersAsync().then((response) => {
     response.forEach((filter) => {
-      
+
       const categoricalFilter = filter as marks.CategoricalFilter;
 
       categoricalFilter.appliedValues.forEach(function (value) {
         console.log(value.formattedValue + ', ');
       });
-      
+
 
 
     })
@@ -73,25 +73,63 @@ function getFilterDetails(worksheet: marks.Worksheet): void {
 
 
 function MainComponent() {
-  
+
   const [workSheetName, setWorkSheetName] = React.useState<string | null>(null);
   const networkContainer = React.useRef<HTMLDivElement | null>(null);
-  
+
   //const [workSheetSize, setWorkSheetSize] = React.useState<Sheet.Size | null>(null); // use to fit the Viz.
 
 
 
   useEffect(() => {
-    
-    if (typeof window !== 'undefined' && tableau ) {
-       const container = document.createElement('div');
+
+    if (typeof window !== 'undefined' && tableau) {
+      const container = document.createElement('div');
 
 
       tableau.extensions.initializeAsync().then(() => {
         const worksheet = tableau.extensions.worksheetContent?.worksheet
         let worksheetname = null;
         //getDataColumns(worksheet!);
-        
+
+
+        getFilterDetails(worksheet!);
+        const tableaData = getData(worksheet!);
+
+        const nodes: { id: string | undefined; label: string | undefined; }[] = [];
+        tableaData.forEach((value) => {
+          nodes.push({ id: value.from, label: value.from });
+        });
+
+        const edges = tableaData;
+        console.log('edges', edges);
+        // Data for network
+        const data: Data = { nodes, edges };
+
+        // Network options
+        const options: Options = {
+          nodes: {
+            shape: 'dot',
+            size: 16,
+            color: {
+              background: '#97C2FC',
+              border: '#2B7CE9',
+            },
+            font: { color: '#343434' },
+          },
+          edges: {
+            color: '#848484',
+          },
+          physics: {
+            enabled: true,
+          },
+        };
+
+
+        const network = new Network(container, data, options);
+        networkContainer.current = container;
+        network.redraw();
+
 
 
         if (worksheet) {
@@ -99,13 +137,13 @@ function MainComponent() {
             console.log(filterChangedEvent)
             getFilterDetails(worksheet!);
             const tableaData = getData(worksheet!);
-            
+
             const nodes: { id: string | undefined; label: string | undefined; }[] = [];
-            tableaData.forEach((value)=>{
+            tableaData.forEach((value) => {
               nodes.push({ id: value.from, label: value.from });
             });
 
-            const edges = tableaData ;
+            const edges = tableaData;
             console.log('edges', edges);
             // Data for network
             const data: Data = { nodes, edges };
@@ -130,14 +168,14 @@ function MainComponent() {
             };
 
 
-            const network = new Network(container,data,options);
+            const network = new Network(container, data, options);
             networkContainer.current = container;
             network.redraw();
             /*network.on('click', (params) => {
               console.log('Clicked on:', params);
             });*/
-            
-            
+
+
           })
         }
 
@@ -169,13 +207,13 @@ function MainComponent() {
   };
 
   return (
-    
+
     <>
       <Script src="/scripts/tableau.extensions.1.latest.js" strategy="beforeInteractive" />
-     
-    <div ref={networkContainer}
-      style={{ width: '600px', height: '400px', border: '1px solid black' }}
-    />
+
+      <div ref={networkContainer}
+        style={{ width: '600px', height: '400px', border: '1px solid black' }}
+      />
       {renderSheet()}
     </>
   );
